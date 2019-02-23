5e6526b87d678f80e13f051a9c1278ce2f317a04
crislawrence <criswlawrence@gmail.com>
2019-02-23 11:36:28 -0500

    Properly checking mime types and allowing gzipped files

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
-ALLOWED_EXTENSIONS = frozenset(['txt', 'csv', 'xlsx'])
-ALLOWED_MIME_TYPES = frozenset(['text/plain', 'text/csv', 'application/vnd.ms-excel'])
+ALLOWED_EXTENSIONS = frozenset(['txt', 'csv', 'xlsx', 'gz'])
+ALLOWED_MIME_TYPES = frozenset(['text/plain', 'text/csv', 'application/vnd.ms-excel',
+                                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                'application/x-gzip'])
+COMPRESSED_MIME_TYPES = frozenset(['application/x-gzip'])
