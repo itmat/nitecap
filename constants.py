b5407d7172b1f2fdead7d486023025d87072690a
crislawrence <criswlawrence@gmail.com>
2019-03-04 11:12:28 -0500

    Added homepage

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-ALLOWED_EXTENSIONS = frozenset(['txt', 'csv', 'xlsx', 'gz'])
+ALLOWED_EXTENSIONS = frozenset(['txt', 'tsv', 'csv', 'xls', 'xlsx', 'gz'])
 ALLOWED_MIME_TYPES = frozenset(['text/plain', 'text/csv', 'application/vnd.ms-excel',
                                 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                                 'application/x-gzip', 'application/gzip'])
