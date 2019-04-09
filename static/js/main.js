a8963c4f8c6af183e24fad8ed99bdfe82ecd8fab
crislawrence <criswlawrence@gmail.com>
2019-04-09 14:52:05 -0400

    Made a working popover that also works on Chrome

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     // Needed to make popovers work
-    $("[data-toggle=popover]").popover();
+    initializePopovers();
 
     setCopyrightYear();
 });
@@ -10,4 +10,23 @@ $(document).ready(function () {
 // Sets copyright year
 function setCopyrightYear() {
     $('#copyrightYear').html(new Date().getFullYear());
+}
+
+// Initialize popovers
+function initializePopovers() {
+    var el = $('[data-toggle="popover"]');
+    el.on('click', function(e){
+        var el = $(this);
+        setTimeout(function(){
+            el.popover('show');
+        }, 200); // Must occur after document click event below.
+    })
+    .on('shown.bs.popover', function(){
+        $(document).on('click.popover', function() {
+            el.popover('hide'); // Hides all
+        });
+    })
+    .on('hide.bs.popover', function(){
+        $(document).off('click.popover');
+    });
 }
