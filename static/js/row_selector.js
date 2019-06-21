7185656beeee4502b267185b4c006bec2fadfb4f
Thomas Brooks <thobr@sas.upenn.edu>
2019-06-21 13:39:13 -0400

    Vue-ify row selector into a component

---
+++ static/js/row_selector.js
@@ -0,0 +1,136 @@
+Vue.component('row-selector', {
+    data: function () {
+        return {
+            top: 0,
+        };
+    },
+
+    props: {
+        selectedRow: Number,
+        numRows: Number,
+        sortOrder: Array,
+        filtered: Array,
+        selectRow: Function,
+        labels: Array,
+        labelMaxLength: Number,
+        qValues: Array,
+    },
+
+    methods: {
+        goDown: function (event) {
+            // Find the next row down that is unfiltered
+            var i = this.selectedRow+1;
+            while (true) {
+                if (i >= this.labels.length) {
+                    // End of the table, do nothing;
+                    break;
+                }
+                if (!this.filtered[this.sortOrder[i]]) {
+                    this.selectRow(i);
+                    break;
+                }
+                i++;
+            }
+        },
+
+        goUp: function () {
+            // Find the next row up that is unfiltered
+            var i = this.selectedRow-1;
+            while (true) {
+                if (i < 0) {
+                    // Top of the table, do nothing;
+                    break;
+                }
+                if (!this.filtered[this.sortOrder[i]]) {
+                    this.selectRow(i);
+                    break;
+                }
+                i--;
+            }
+        },
+
+        pageDown: function () {
+            var max_top = Math.max(0, this.labels.length - this.numRows);
+            this.top = Math.min(this.top + this.numRows, max_top);
+        },
+
+        pageUp: function () {
+            this.top = Math.max(this.top - this.numRows, 0);
+        },
+    },
+
+    computed: {
+        rows: function () {
+            let vm = this;
+            let rows_ = [];
+            for(let i = 0; i < vm.numRows; i++) {
+                let idx = vm.sortOrder[i + vm.top];
+                if (idx > vm.fullLabels.length) {
+                    return {label: '-', filtered: true, selected: false, index: idx};
+                }
+
+                rows_[i] = {
+                    label: vm.fullLabels[idx],
+                    filtered: vm.filtered[idx],
+                    selected: vm.selectedRow == (i + vm.top),
+                    index: idx,
+                };
+            }
+            return rows_;
+        },
+
+        fullLabels: function () {
+            let vm = this;
+            let desired_label_length = Math.max.apply(0, vm.labels.map( function (x) {return x.length;}));
+            desired_label_length = Math.min(desired_label_length, vm.labelMaxLength);
+
+            return vm.labels.map( function(label, i) {
+                let q = vm.qValues[i];
+                label = String(label).slice(0,vm.labelMaxLength);
+                return padEnd(label, desired_label_length, ' ') + ' Q: ' + toFixed(vm.qValues[i], 2);
+            });
+        },
+    },
+
+    watch: {
+        "selectedRow": function () {
+            let row_index = this.selectedRow;
+            // Update row selector scrolling
+            if (row_index >= this.top) {
+                if (row_index < this.top + this.numRows) {
+                    // Do nothing, already can see the right row
+                } else {
+                    // Moving down, put it at the bottom
+                    this.top = row_index - this.numRows + 1;
+                }
+            } else {
+                // Moving up, put it at the top
+                this.top = row_index;
+            }
+        },
+    },
+
+    mounted: function () {
+        let vm = this;
+        vm.$el.addEventListener('wheel', function (event) {
+            if (event.deltaY > 0) {
+                var max_top = Math.max(0, vm.labels.length - vm.numRows);
+                vm.top = Math.min(vm.top + 3, max_top) ;
+            } else if (event.deltaY < 0) {
+                vm.top = Math.max(vm.top - 3, 0) ;
+            }
+
+            event.preventDefault();
+        }, {passive: false}); // indicate that we will prevent default, true may later become the default
+    },
+
+    template: "<div class='list-group row-selector' tabindex='0'\
+                   v-on:keydown.down.prevent='goDown' v-on:keydown.up.prevent='goUp' \
+                   v-on:keydown.page-up.prevent='pageUp' v-on:keydown.page-down.prevent='pageDown'> \
+       <div v-for='row in rows' v-bind:disabled='row.filtered' \
+            v-bind:class='\"list-group-item list-group-item-action py-0 \" + (row.selected ? \"active\" : \"\")' \
+            v-on:click='selectRow(row.index)' \
+            v-bind:key='row.index'\
+            >{{row.label}}</div> \
+    </div>",
+});
