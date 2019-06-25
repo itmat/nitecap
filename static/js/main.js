ec272ffb27e3e577992ee93f87e6a9b56296db1e
Thomas Brooks <thobr@sas.upenn.edu>
2019-06-25 15:30:16 -0400

    Fix tooltips with Vue

--- static/js/main.js
+++ static/js/main.js
@@ -14,20 +14,10 @@ function setCopyrightYear() {
 
 // Initialize popovers
 function initializePopovers() {
-    var el = $('.help-pointer[data-toggle="popover"]');
-    el.on('click', function(e){
-        var el = $(this);
-        setTimeout(function(){
-            el.popover('show');
-        }, 200); // Must occur after document click event below.
-    })
-    .on('shown.bs.popover', function(){
-        $(document).on('click.popover', function() {
-            el.popover('hide'); // Hides all
-        });
-    })
-    .on('hide.bs.popover', function(){
-        $(document).off('click.popover');
+    $('body').popover({
+        selector: '[data-toggle="popover"]',
+        placement: 'top',
+        trigger: 'hover',
     });
 }
 
