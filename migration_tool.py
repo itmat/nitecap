2c31e70c54b62886f1c852d651abe11a62309002
crislawrence <criswlawrence@gmail.com>
2019-06-28 14:05:33 -0400

    Fixed migration tool

--- migration_tool.py
+++ migration_tool.py
@@ -2,6 +2,7 @@ import os
 import re
 import sqlite3
 
+import termcolor as termcolor
 from dotenv import load_dotenv, find_dotenv
 
 
@@ -18,78 +19,138 @@ def migrate(rehearse, db):
         for datum in data:
             user_id, spreadsheet_id, uploaded_file_path, processed_file_path = datum
             print(f"Migrating data for spreadsheet_id {spreadsheet_id} belonging to user_id {user_id}")
-            print(f"\tOriginal uploaded file path: {uploaded_file_path}")
-            print(f"\tOriginal processed file path: {processed_file_path}")
             user_folder = os.path.join(os.environ['UPLOAD_FOLDER'], f'user_{user_id}')
+
+            # Prepare spreadsheet data folder
+            print("\tSpreadsheet data folder")
             spreadsheet_data_folder = os.path.join(user_folder, f'spreadsheet_{spreadsheet_id}')
+
+            # Create spreadsheet data folder
             if not os.path.exists(spreadsheet_data_folder):
-                if rehearse:
-                    print(f"\t\tMkdir - {spreadsheet_data_folder}")
-                else:
+                if not rehearse:
                     os.makedirs(spreadsheet_data_folder, exist_ok=True)
-            uploaded_file_ext = os.path.basename(os.path.splitext(uploaded_file_path)[1])
-            processed_file_ext = os.path.basename(os.path.splitext(processed_file_path)[1])
-            new_uploaded_file_path = os.path.join(spreadsheet_data_folder, f"uploaded_spreadsheet.{uploaded_file_ext}")
-            new_processed_file_path = os.path.join(spreadsheet_data_folder, f"processed_spreadsheet.{processed_file_ext}")
-            if rehearse:
-                print(f"\t\tUPDATE spreadsheets SET spreadsheet_data_path = {spreadsheet_data_folder} WHERE id = {spreadsheet_id}")
-                print(f"\t\tmv {uploaded_file_path} to {new_uploaded_file_path}")
-                print(f"\t\tmv {processed_file_path} to {new_processed_file_path}")
-                print(160*"-")
-            else:
+                print(f"\t\tMkdir - {spreadsheet_data_folder}")
+
+            # Add spreadsheet data folder to spreadsheet record
+            if not rehearse:
                 sql = 'UPDATE spreadsheets SET spreadsheet_data_path = ? WHERE id = ?'
                 cursor.execute(sql, (spreadsheet_data_folder, spreadsheet_id))
-                os.rename(uploaded_file_path, new_uploaded_file_path)
-                os.rename(processed_file_path, new_processed_file_path)
+            print(f"\t\tUPDATE spreadsheets SET spreadsheet_data_path = {spreadsheet_data_folder} "
+                  f"WHERE id = {spreadsheet_id}")
+
+            # Check for related uploaded spreadsheet file and if present, move it under new spreadsheet folder
+            print(f"\tOriginal uploaded file path: {uploaded_file_path}")
+            if uploaded_file_path and os.path.exists(uploaded_file_path):
+                uploaded_file_ext = os.path.basename(os.path.splitext(uploaded_file_path)[1])
+                new_uploaded_file_path = os.path.join(spreadsheet_data_folder,
+                                                      f"uploaded_spreadsheet.{uploaded_file_ext}")
+                # Relocate uploaded spreadsheet
+                if not rehearse:
+                    os.rename(uploaded_file_path, new_uploaded_file_path)
+                print(f"\t\tmv {uploaded_file_path} to {new_uploaded_file_path}")
+
+                # Add new upload file path to spreadsheet record
+                if not rehearse:
+                    sql = 'UPDATE spreadsheets SET uploaded_file_path = ? WHERE id = ?'
+                    cursor.execute(sql, (new_uploaded_file_path, spreadsheet_id))
+                print(f"\t\tUPDATE spreadsheets SET uploaded_file_path = {new_uploaded_file_path} "
+                      f"WHERE id = {spreadsheet_id}")
+
+            else:
+                termcolor.cprint("\t\tMissing uploaded spreadsheet file or reference in record", 'red')
+
+            # Check for related processed spreadsheet file and if present, move it under new spreadsheet folder
+            print(f"\tOriginal processed file path: {processed_file_path}")
+            if processed_file_path and os.path.exists(processed_file_path):
+                processed_file_ext = os.path.basename(os.path.splitext(processed_file_path)[1])
+                new_processed_file_path = os.path.join(spreadsheet_data_folder,
+                                                       f"processed_spreadsheet.{processed_file_ext}")
+
+                # Relocate processed spreadsheet
+                if not rehearse:
+                    os.rename(processed_file_path, new_processed_file_path)
+                print(f"\t\tmv {processed_file_path} to {new_processed_file_path}")
+
+                # Add new processed file path to spreadsheet record
+                if not rehearse:
+                    sql = 'UPDATE spreadsheets SET file_path = ? WHERE id = ?'
+                    cursor.execute(sql, (new_processed_file_path, spreadsheet_id))
+                print(
+                    f"\t\tUPDATE spreadsheets SET file_path = {new_processed_file_path} WHERE id = {spreadsheet_id}")
+
+            else:
+                termcolor.cprint("\t\tMissing processed spreadsheet file or reference in record", 'red')
+
+            print(180 * "-")
             relocated_files.extend([os.path.basename(uploaded_file_path), os.path.basename(processed_file_path)])
 
+    # Handle remaining top level files
     print("")
     print("Handling those files not directly identifiable via the db...")
     pattern = re.compile('^(\d+)v(\d+)\.comparison.*$')
-    for filepath in os.listdir(upload_folder):
-        filename = os.path.basename(filepath)
-        if filename in relocated_files:
-            if not rehearse:
-                print(f"\tFile {filename} should have been relocated!")
+    for filename in os.listdir(upload_folder):
+
+        filepath = os.path.join(upload_folder, filename)
+
+        # Skip over directories
+        if os.path.isdir(filepath):
             continue
-        print(160 * "-")
+
+        # If the file was supposed to have been relocated, note it and bypass it.  Skip for rehearse since that doesn't
+        # really change anything.
+        if filepath in relocated_files:
+            if not rehearse:
+                termcolor.cprint(f"\tFile {filepath} should have been relocated!", 'red')
+            continue
+        print(180 * "-")
+
+        # Look for comparison files
         print(f"Evaluating {filepath}")
         result = re.match(pattern, filename)
+
         if result:
+
+            # Got a comparison file but do the related spreadsheets still exist?  If so, do not move it anywhere.
             sql = 'SELECT user_id FROM spreadsheets WHERE id = ?'
             cursor.execute(sql, (result.group(1),))
             user_id = cursor.fetchone()
             if user_id:
                 user_id = user_id[0]
             else:
-                print(f"\tSpreadsheet id {result.group(1)} no longer exists for {filename}")
+                termcolor.cprint(f"\tSpreadsheet id {result.group(1)} no longer exists for {filename}", 'red')
                 continue
             cursor.execute(sql, (result.group(2),))
             user_id_repeat = cursor.fetchone()
             if user_id_repeat:
                 user_id_repeat = user_id_repeat[0]
             else:
-                print(f"\tSpreadsheet id {result.group(2)} no longer exists for {filename}")
+                termcolor.cprint(f"\tSpreadsheet id {result.group(2)} no longer exists for {filename}", 'red')
                 continue
+
+            # Check if the spreadsheets are both owned by the same user.  If not, do not move the comparison file
             if user_id != user_id_repeat:
-                print(f"\tDifferent users ({user_id},{user_id_repeat}) own the different spreadsheets"
-                      f" ({result.group(1), result.group(2)}) in {filename}.  No action taken.")
+                termcolor.cprint(f"\tDifferent users ({user_id},{user_id_repeat}) own the different spreadsheets"
+                                 f" ({result.group(1), result.group(2)}) in {filename}.  No action taken.", 'red')
                 continue
             else:
                 user_folder = os.path.join(upload_folder, f"user_{user_id}")
+
+                # Create a comparisons folder for the user if needed
                 comparison_folder = os.path.join(user_folder, "comparisons")
                 if not os.path.exists(comparison_folder):
-                    if rehearse:
-                        print(f"\tMkdir - {comparison_folder}")
-                    else:
+                    if not rehearse:
                         os.makedirs(comparison_folder, exist_ok=True)
-                new_filepath = os.path.join(comparison_folder, filename)
-                if rehearse:
-                    print(f"\tmv {filepath} to {new_filepath}")
-                #os.rename(filepath, new_filepath)
-        else:
-            print(f"\tFile {filepath} has no owner.  May be legecy data.")
+                    print(f"\tMkdir - {comparison_folder}")
 
+                # Relocate the comparisons file
+                new_filepath = os.path.join(comparison_folder, filename)
+                if not rehearse:
+                    os.rename(filepath, new_filepath)
+                print(f"\tmv {filepath} to {new_filepath}")
+
+        else:
+            # A non-comparison file with no reference
+            termcolor.cprint(f"\tFile {filepath} has no owner.  May be legecy data.", 'red')
 
 
 if __name__ == "__main__":
