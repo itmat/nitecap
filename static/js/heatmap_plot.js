bb5f9789a39340869b67b4d5626289ff6e22ea5a
Thomas Brooks <thobr@sas.upenn.edu>
2019-07-09 12:59:14 -0400

    Move heatmap to own Vue component

---
+++ static/js/heatmap_plot.js
@@ -0,0 +1,234 @@
+Vue.component("heatmap-plot", { 
+    data: function () {
+        return {
+            cutoff: 0,
+            rendered: false,
+            data: [],
+            x_values: [],
+            x_labels: [],
+            heatmap_labels: [],
+            config: {
+                fold_days: false,
+                combine_replicates: false,
+                show_labels: false,
+            },
+        };
+    },
+
+    props: {
+        spreadsheets: Array,
+        num_days: Number,
+        selected_rows: Array,
+        labels: Array,
+    },
+
+    methods: {
+        updateHeatmap: function() {
+            let vm = this;
+
+            vm.cutoff = vm.selected_rows.length;
+
+            let phase_sorted_order = vm.selected_rows.sort( function (i,j) {
+                return compare(vm.spreadsheets[0].peak_times[i], vm.spreadsheets[0].peak_times[j], i,j);
+            } );
+
+            let zScoreData = vm.spreadsheets.map(function(spreadsheet) {
+                return computeZScores(phase_sorted_order)(spreadsheet.data);
+            });
+
+            let times = vm.spreadsheets.map(function(spreadsheet){ return spreadsheet.x_values; });
+
+            if (vm.config.fold_days) {
+                times = vm.spreadsheets.map(function(spreadsheet) {
+                    return spreadsheet.x_values.map(function(x) {
+                        return x % spreadsheet.timepoints_per_day;
+                    });
+                });
+            }
+
+            vm.heatmap_labels = phase_sorted_order.map( function(i) {
+                return vm.labels[i];
+            } );
+
+            let heatmap_options = {
+                modeBarButtonsToAdd: [{
+                    name: 'download in SVG format',
+                    icon: Plotly.Icons.camera,
+                    click: function (gd) {
+                        Plotly.downloadImage(gd, {format: 'svg'})
+                    }
+                }]
+            };
+
+            if (vm.config.combine_replicates) {
+                // Average all z-scores at the same timepoint together
+                let meanZScores = zScoreData.map( function(zscores,idx) {
+                    return meanByTimepoints(zscores, times[idx]);
+                });
+
+                vm.data = meanZScores;
+
+                if (vm.config.fold_days) {
+                    vm.x_values = meanZScores.map( function (zscores) {
+                        return zscores[0].map( function(x,i) {return "Timepoint " + (i + 1);});
+                    });
+                } else {
+                    vm.x_values = vm.spreadsheets.map( function(spreadsheet) {return spreadsheet.x_labels;} );
+                }
+            } else {
+                if (vm.config.fold_days) {
+                    let x_sort_order = times.map( function(times_) {
+                        return times_.map( function(x,i) {return i;} ).sort( function (i,j) {
+                            return compare(times_[i],times_[j],i,j);
+                        } );
+                    });
+
+                    vm.data = zScoreData.map( function(zscores,idx) {
+                        return zscores.map( function(row) {
+                            return x_sort_order[idx].map( function(i) {
+                                return row[i];
+                            });
+                        } );
+                    });
+
+                    let rep_counts = [];
+                    vm.x_values = x_sort_order.map( function(x_sort_order_, idx) {
+                        return x_sort_order_.map( function(i) {
+                            let time = times[idx][i];
+                            if (rep_counts[time] === undefined) {
+                                rep_counts[time] = 0;
+                            }
+                            rep_counts[time] += 1;
+
+                            return "Timepoint " + (time + 1) + " Rep " + rep_counts[time];
+                        });
+                    });
+                } else {
+                    vm.data = zScoreData;
+
+                    vm.x_values = vm.spreadsheets.map( function(spreadsheet, idx) {
+                        let rep_counts = [];
+                        return spreadsheet.x_values.map( function(time) {
+                            if (rep_counts[time] === undefined) {
+                                rep_counts[time] = 0;
+                            }
+                            rep_counts[time] += 1;
+                            let day = Math.floor(time / spreadsheet.timepoints_per_day) + 1;
+                            let time_of_day = time % spreadsheet.timepoints_per_day + 1;
+                            return "Day " + day + " Timepoint " + time_of_day + " Rep " + rep_counts[time];
+                        });
+                    });
+                }
+            }
+
+            // Find max/min values so that we can center the colormap with 0=middle
+            let maxInRow = vm.data.map( function(spreadsheet) { return spreadsheet.map(array_max); });
+            let minInRow = vm.data.map( function(spreadsheet) { return spreadsheet.map(array_min); });
+
+            let max = Math.max.apply(null, maxInRow.map(array_max));
+            let min = Math.min.apply(null, minInRow.map(array_min));
+            let limit = Math.max(Math.abs(min), Math.abs(max));
+
+            let y_vals =  vm.heatmap_labels.map(function (x,i) {return i;});
+
+            let heatmap_values = vm.spreadsheets.map( function(spreadsheet, idx) {
+                let colorbar = {};
+                if (idx === 0){
+                    colorbar = {
+                        title: 'Z Score',
+                        titleside: 'right'
+                    };
+                }
+                return {
+                    x: vm.x_values[idx],
+                    y: y_vals,
+                    z: vm.data[idx],
+                    type: 'heatmap',
+                    xaxis: "x" + (idx+1),
+                    yaxis: "y",
+                    zmin: -1 * limit,
+                    zmax: limit,
+                    colorbar: colorbar
+                };
+            });
+
+            let heatmap_layout = {
+                height: 700,
+                width: 300*vm.spreadsheets.length+600,
+                margin: {
+                    l: 200,
+                    r: 5,
+                    b: 175,
+                    t: 20
+                },
+                pad: 4,
+                yaxis: {
+                    tickmode: "array",
+                    ticktext: vm.heatmap_labels,
+                    tickvals: y_vals,
+                    showticklabels: vm.config.show_labels,
+                    ticks: ''
+                },
+                grid: {
+                    rows: 1,
+                    columns: vm.spreadsheets.length
+                }
+            };
+
+            Plotly.newPlot('heatmap', heatmap_values, heatmap_layout, heatmap_options);
+
+            vm.rendered = true;
+        },
+
+        downloadHeatmap: function() {
+            let gd = document.getElementById('heatmap');
+            Plotly.downloadImage(gd, {format: 'svg', filename: 'heatmap_' + (this.selected_rows.length)});
+        },
+    },
+
+    watch: {
+        "config": {
+            handler: "updateHeatmap",
+            deep: true
+        },
+    },
+    
+    template: '\
+    <div class="row">\
+        <div class="col card" id="heatmap_block">\
+            <div class="card-body">\
+                <div class="row" style="padding-top:2rem;padding-bottom:2rem">\
+                    <span class="col-lg-5">\
+                        <input type="hidden" id="row_index" name="row_index"/>\
+                        <button class="btn btn-primary" v-on:click="updateHeatmap">Generate Heatmap</button>\
+                    </span>\
+                </div>\
+\
+                <div v-show="rendered">\
+                    <span class="col-lg-12">Number of items in heatmap: {{heatmap_labels.length}}</span>\
+                    <div id="heatmap"></div>\
+                    <span class="col-lg-5">\
+                        <div class="form-check">\
+                            <input class="form-check-input" id="combine_replicates" type="checkbox" v-model="config.combine_replicates">\
+                            <label class="form-check-label" for="combine_replicates">Combine replicates</label>\
+                        </div>\
+    \
+                        <div class="form-check" v-if="num_days > 1">\
+                            <input class="form-check-input" id="heatmap_fold_days" type="checkbox" v-model="config.fold_days">\
+                            <label class="form-check-label" for="heatmap_fold_days">Overlay cycles</label>\
+                        </div>\
+    \
+                        <div class="form-check">\
+                            <input class="form-check-input" id="heatmap_show_labels" type="checkbox" v-model="config.show_labels">\
+                            <label class="form-check-label" for="heatmap_show_labels">Show row labels</label>\
+                        </div>\
+                    </span>\
+                    <span>\
+                        <button id="download_heatmap" class="btn btn-primary" v-on:click="downloadHeatmap">Download Heatmap</button>\
+                    </span>\
+                </div> \
+            </div>\
+        </div>\
+    </div>\
+    ',
+});
