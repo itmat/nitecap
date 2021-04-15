e4828d44535d5d909680137f4de91ecc4b6d923c
Thomas Brooks <tgbrooks@gmail.com>
2021-04-15 14:57:07 -0400

    Allow csv files on server
    On our server, but not dev setups, csv files give a MIME type of
    application/csv instead of text/csv, apparently.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 ALLOWED_EXTENSIONS = frozenset(['.txt', '.tsv', '.csv', '.xls', '.xlsx', '.gz'])
 ALLOWED_MIME_TYPES = frozenset(['text/plain', 'text/csv', 'application/vnd.ms-excel',
                                 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                                'application/x-gzip', 'application/gzip'])
+                                'application/x-gzip', 'application/gzip', 'application/csv'])
 COMPRESSED_MIME_TYPES = frozenset(['application/x-gzip', 'application/gzip'])
 EXCEL_MIME_TYPES = frozenset(['application/vnd.ms-excel',
                               'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'])
