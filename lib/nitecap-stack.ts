44967df8e61e18f3b821d3552dc7389b608d3874
Antonijo Mrčela <antonijo.mrcela@pennmedicine.upenn.edu>
2021-05-03 12:45:54 -0400

    Added the spreadsheet bucket

--- lib/nitecap-stack.ts
+++ lib/nitecap-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from "@aws-cdk/aws-s3";
 
 export class NitecapStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    let spreadsheetBucket = new s3.Bucket(this, "SpreadsheetBucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      lifecycleRules: [
+        {
+          transitions: [
+            {
+              transitionAfter: cdk.Duration.days(90),
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+            },
+          ],
+        },
+      ],
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
