870c6a02d122c3f469bd46f06648c501d4ee6bc8
Antonijo Mrčela <antonijo.mrcela@pennmedicine.upenn.edu>
2021-05-03 12:58:40 -0400

    Added the notification API

--- lib/nitecap-stack.ts
+++ lib/nitecap-stack.ts
@@ -1,6 +1,10 @@
+import * as apigateway from "@aws-cdk/aws-apigatewayv2";
 import * as cdk from "@aws-cdk/core";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as iam from "@aws-cdk/aws-iam";
 import * as s3 from "@aws-cdk/aws-s3";
+
+import { CfnAccount as ApiGatewayCfnAccount } from "@aws-cdk/aws-apigateway";
 
 export class NitecapStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -40,5 +44,127 @@ export class NitecapStack extends cdk.Stack {
       partitionKey: { name: userId, type: dynamodb.AttributeType.STRING },
       projectionType: dynamodb.ProjectionType.KEYS_ONLY,
     });
+
+    // Notification API
+
+    let notificationApi = new apigateway.CfnApi(this, "NotificationApi", {
+      name: "NotificationApi",
+      protocolType: "WEBSOCKET",
+      routeSelectionExpression: "$request.body.action",
+    });
+
+    let notificationApiRole = new iam.Role(this, "NotificationApiRole", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      managedPolicies: [
+        {
+          managedPolicyArn:
+            "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs",
+        },
+      ],
+    });
+
+    new ApiGatewayCfnAccount(this, "NotificationApiAccount", {
+      cloudWatchRoleArn: notificationApiRole.roleArn,
+    });
+
+    notificationApiRole.attachInlinePolicy(
+      new iam.Policy(this, "NotificationApiPolicy", {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["dynamodb:UpdateItem", "dynamodb:DeleteItem"],
+            resources: [connectionTable.tableArn],
+          }),
+        ],
+      })
+    );
+
+    let disconnectIntegration = new apigateway.CfnIntegration(
+      this,
+      "DisconnectIntegration",
+      {
+        apiId: notificationApi.ref,
+        integrationType: "AWS",
+        integrationMethod: "POST",
+        integrationUri: `arn:aws:apigateway:${this.region}:dynamodb:action/DeleteItem`,
+        templateSelectionExpression: "\\$default",
+        credentialsArn: notificationApiRole.roleArn,
+        requestTemplates: {
+          "\\$default": JSON.stringify({
+            TableName: connectionTable.tableName,
+            Key: {
+              connectionId: { S: "$context.connectionId" },
+            },
+          }),
+        },
+      }
+    );
+
+    let notificationApiDisconnectRoute = new apigateway.CfnRoute(
+      this,
+      "DisconnectRoute",
+      {
+        apiId: notificationApi.ref,
+        routeKey: "$disconnect",
+        target: `integrations/${disconnectIntegration.ref}`,
+      }
+    );
+
+    let defaultIntegration = new apigateway.CfnIntegration(
+      this,
+      "DefaultIntegration",
+      {
+        apiId: notificationApi.ref,
+        integrationType: "AWS",
+        integrationMethod: "POST",
+        integrationUri: `arn:aws:apigateway:${this.region}:dynamodb:action/UpdateItem`,
+        templateSelectionExpression: "\\$default",
+        credentialsArn: notificationApiRole.roleArn,
+        requestTemplates: {
+          "\\$default": JSON.stringify({
+            TableName: connectionTable.tableName,
+            Key: { connectionId: { S: "$context.connectionId" } },
+            UpdateExpression: `SET ${userId} = :value`,
+            ExpressionAttributeValues: { ":value": { S: "$input.body" } },
+          }),
+        },
+      }
+    );
+
+    let notificationApiDefaultRoute = new apigateway.CfnRoute(
+      this,
+      "DefaultRoute",
+      {
+        apiId: notificationApi.ref,
+        routeKey: "$default",
+        target: `integrations/${defaultIntegration.ref}`,
+      }
+    );
+
+    let notificationApiStage = new apigateway.CfnStage(
+      this,
+      "NotificationApiStage",
+      {
+        apiId: notificationApi.ref,
+        stageName: "default",
+        defaultRouteSettings: {
+          dataTraceEnabled: true,
+          loggingLevel: "ERROR",
+          detailedMetricsEnabled: false,
+        },
+      }
+    );
+
+    let notificationApiDeployment = new apigateway.CfnDeployment(
+      this,
+      "NotificationApiDeployment",
+      {
+        apiId: notificationApi.ref,
+        stageName: notificationApiStage.stageName,
+      }
+    );
+
+    notificationApiDeployment.addDependsOn(notificationApiDisconnectRoute);
+    notificationApiDeployment.addDependsOn(notificationApiDefaultRoute);
   }
 }
