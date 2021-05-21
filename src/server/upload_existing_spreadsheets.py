438d42b62a46a0ade02b093c69cc9394f6a0aef5
Thomas Brooks <tgbrooks@gmail.com>
2021-05-21 17:04:53 -0400

    Skip over spreadsheets from visitors

--- src/server/upload_existing_spreadsheets.py
+++ src/server/upload_existing_spreadsheets.py
@@ -7,4 +7,7 @@ db.init_app(app.app)
 
 with app.app.app_context():
     for spreadsheet in db.session.query(Spreadsheet).order_by(Spreadsheet.id):
+        if spreadsheet.user.visitor:
+            print(f"Skipping over spreadsheet {spreadsheet.id} from user {spreadsheet.user_id} since user is visitor")
+            continue
         print(f"Spreadsheet ID {spreadsheet.id} from user {spreadsheet.user_id}: {spreadsheet.descriptive_name}")
