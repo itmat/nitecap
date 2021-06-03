25f50337a3c87d6cc86f9857f93169a8f2142c07
Antonijo Mrčela <antonijo.mrcela@pennmedicine.upenn.edu>
2021-06-03 10:55:17 -0400

    Updated tests

--- cypress/integration/visitors.spec.ts
+++ cypress/integration/visitors.spec.ts
@@ -2,10 +2,10 @@ import environment from "./.env";
 
 describe("Standard visitors workflow", () => {
   it("loads a spreadsheets and displays the computation results", () => {
-    cy.visit(environment.URL);
+    cy.visit(environment.baseUrl);
     cy.contains("Load new spreadsheet").click();
     cy.get("input[type='file']").attachFile("spreadsheet.tsv");
-    cy.get("#submit_btn").click();
+    cy.contains("Submit").click();
     cy.get("body").should("contain", "Collect Data");
     cy.contains("div", "Number of total timepoints")
       .find("input")
@@ -16,5 +16,18 @@ describe("Standard visitors workflow", () => {
       .type("6")
       .blur();
     cy.contains("Submit").click();
+    cy.get("body").should("contain", "Loading...");
+    cy.get("body").should("contain", "Spreadsheet Rows");
+    cy.contains("tr>td", "LS")
+      .next()
+      .contains("Unknown")
+      .contains("PENDING")
+      .contains("100%", { timeout: 10000 });
+    cy.contains("tr>td", "LS")
+      .next()
+      .should(
+        "have.text",
+        "\n                                                        p: 9.5e-4\n                                                    "
+      );
   });
 });
