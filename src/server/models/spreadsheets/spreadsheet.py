62d7c6762e3e1b7a2377846e7f0f0e6596d301bd
Thomas Brooks <tgbrooks@gmail.com>
2021-06-21 16:47:50 -0400

    Revert "Add AUTOINCREMENT to user and spreadheet ids"
    This reverts commit 0bf40276bcea5afe155e53c163b9cd303dd2c20d.

--- src/server/models/spreadsheets/spreadsheet.py
+++ src/server/models/spreadsheets/spreadsheet.py
@@ -35,9 +35,6 @@ MAX_JTK_COLUMNS = 85
 
 class Spreadsheet(db.Model):
     __tablename__ = "spreadsheets"
-    __table_args__ = {
-        "sqlite_autoincrement": True
-    }
     id = db.Column(db.Integer, primary_key=True)
     descriptive_name = db.Column(db.String(250), nullable=False)
     num_timepoints = db.Column(db.Integer)
