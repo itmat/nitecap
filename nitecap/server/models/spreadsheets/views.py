25fa89ab0065e808daa5f09886511cef40811bf3
Antonijo Mrčela <antonijo.mrcela@pennmedicine.upenn.edu>
2023-02-08 00:02:38 +0000

    Retire old share token system

--- nitecap/server/models/spreadsheets/views.py
+++ nitecap/server/models/spreadsheets/views.py
@@ -16,7 +16,6 @@ from flask import Blueprint, request, session, url_for, redirect, render_templat
 from flask import current_app
 import simplejson
 from sklearn.decomposition import PCA
-from itsdangerous import JSONWebSignatureSerializer as Serializer
 
 import constants
 from exceptions import NitecapException
@@ -573,38 +572,6 @@ def consume_share(token):
 
     errors = []
     share = Share.find_by_id(token)
-
-    if share is None:
-        # Try the old share token
-        # TODO at some point this can be deprecated and removed
-        #      We used to create signed tokens encoding share permissions
-        #      and to help users migrate off those, we still check for such tokens
-        #      and point them to the right spreadsheets to make a new share URL
-        #      Share tokens are now stored in the DB instead.
-        try:
-            s = Serializer(os.environ['OLD_SECRET_KEY'])
-            token_value = s.loads(token)
-            user_id = token_value['user_id']
-            spreadsheet_ids = token_value['spreadsheet_ids']
-            valid = True
-        except:
-            valid = False
-
-        if valid:
-            current_app.logger.error(f"User attempted to access an old share token for spreadsheets {spreadsheet_ids} and user_id {user_id}")
-            spreadsheet_ids_str = ','.join(spreadsheet_ids)
-            url = url_for(".show_spreadsheet", spreadsheet_id=spreadsheet_ids_str, _external=True)
-            errors.append("The URL you received no longer works as a share due to system upgrades. "
-                "A permanent share can be obtained from the original spreadsheet's uploader. "
-                "If you are the owner of these spreadsheets, you can generate a new share link at "
-                f'{url}')
-        else:
-            current_app.logger.error(f"Invalid share URL identified with token {token}");
-            errors.append("The URL you received does not work. It may have been mangled in transit. "
-                      "Please request another share")
-        current_app.logger.error(errors)
-        return render_template('spreadsheets/upload_file.html', errors=errors), 404
-
 
     sharing_user = User.find_by_id(share.user_id)
     spreadsheet_ids = share.spreadsheet_ids_str.split(',')
